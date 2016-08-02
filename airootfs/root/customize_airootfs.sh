6ffe6bdb6f5c693f20fa4f513524ef306817288f
Brandon Bielicki <brandonbielicki@gmail.com>
2016-08-02 02:33:03 +0000

    Overriding with new archiso build

---
+++ airootfs/root/customize_airootfs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e -u
+
+sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
+locale-gen
+
+ln -sf /usr/share/zoneinfo/UTC /etc/localtime
+
+if [[ $(getent passwd brandon) != "" ]]; then
+	usermod -p g9iGzNZ624pGs brandon
+else
+	useradd -m -p g9iGzNZ624pGs -s /bin/bash -g users -G wheel brandon
+	echo 'brandon ALL=(ALL:ALL) ALL' >> /etc/sudoers
+fi
+
+usermod -p g9iGzNZ624pGs -s /usr/bin/zsh root
+cp -aT /etc/skel/ /root/
+chmod 700 /root
+
+sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
+sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
+sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
+
+sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
+sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
+sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
+
+systemctl enable pacman-init.service choose-mirror.service
+systemctl set-default multi-user.target
+
